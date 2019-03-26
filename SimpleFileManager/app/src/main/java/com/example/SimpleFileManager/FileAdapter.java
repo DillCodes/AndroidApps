777429750d537024f657a0dcbe2a6004c7129384
DillCodes <48857950+DillCodes@users.noreply.github.com>
2019-03-26 10:08:42 +0800

    Add files via upload

---
+++ SimpleFileManager/app/src/main/java/com/example/SimpleFileManager/FileAdapter.java
@@ -0,0 +1,192 @@
+package com.example.SimpleFileManager;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.support.v4.content.ContextCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class FileAdapter extends BaseAdapter {
+
+    private File currentFolder;
+    private List<File> folders = new ArrayList<>();
+    private List<File> files = new ArrayList<>();
+    private MainActivity activity;
+    private byte offset;
+    private boolean homeFolder;
+
+    private Set<String> selectedItems = new HashSet<>();
+
+
+    FileAdapter(File currentFolder, boolean homeFolder, MainActivity mainActivity){
+        this.currentFolder = currentFolder;
+        this.homeFolder = homeFolder;
+        if(homeFolder && currentFolder.getName().equals(""))
+            offset = 0;
+        else if(homeFolder || currentFolder.getName().equals(""))
+            offset = 1;
+        else
+            offset = 2;
+        this.activity = mainActivity;
+        File[] foldersArray = currentFolder.listFiles(new FileFilter() {
+            @Override
+            public boolean accept(File file) {
+                return file.isDirectory() && !file.getName().startsWith(".");
+            }
+        });
+        if(foldersArray != null)
+            folders = new ArrayList<>(Arrays.asList(foldersArray));
+        File[] filesArray = currentFolder.listFiles(new FileFilter() {
+            @Override
+            public boolean accept(File file) {
+                return !file.isDirectory() && !file.getName().startsWith(".");
+            }
+        });
+        if(filesArray != null)
+            files = new ArrayList<>(Arrays.asList(filesArray));
+        Collections.sort(folders);
+        Collections.sort(files);
+    }
+
+    @Override
+    public int getCount() {
+        return folders.size() + files.size() + offset;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        if(position < offset)
+            return null;
+        else if(position-offset < folders.size())
+            return folders.get(position-offset);
+        else
+            return files.get(position-offset-folders.size());
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            LayoutInflater infalInflater = (LayoutInflater) this.activity
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = infalInflater.inflate(R.layout.file_item, parent, false);
+        }
+
+        ImageView icon = convertView.findViewById(R.id.row_icon);
+        TextView name = convertView.findViewById(R.id.row_name);
+
+        if(position < offset){
+            if(position == 0 && !homeFolder){
+                icon.setImageDrawable(ContextCompat.getDrawable(activity,R.drawable.icon_home));
+                name.setText(activity.getResources().getString(R.string.home));
+                convertView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if(activity.getActionMode() == null)
+                            activity.setDefaultFolder();
+                    }
+                });
+            }
+            else{
+                icon.setImageDrawable(ContextCompat.getDrawable(activity,R.drawable.icon_up));
+                name.setText(activity.getResources().getString(R.string.up));
+                convertView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if(activity.getActionMode() == null)
+                            if(!currentFolder.getName().equals(""))
+                                activity.setCurrentFolder(currentFolder.getParentFile());
+                    }
+                });
+            }
+        }
+        else{
+            File file;
+            if(position-offset < folders.size()){
+                icon.setImageDrawable(ContextCompat.getDrawable(activity,R.drawable.icon_folder));
+                file = folders.get(position-offset);
+            }
+            else{
+                icon.setImageDrawable(ContextCompat.getDrawable(activity,R.drawable.icon_file));
+                file = files.get(position-offset-folders.size());
+            }
+            name.setText(file.getName());
+
+            if(selectedItems.contains(file.getName())){
+                convertView.setBackgroundColor(ContextCompat.getColor(activity,R.color.colorPrimary));
+                name.setTextColor(ContextCompat.getColor(activity,R.color.white));
+            }
+            else {
+                convertView.setBackgroundColor(Color.TRANSPARENT);
+                name.setTextColor(ContextCompat.getColor(activity,R.color.text));
+            }
+        }
+
+        return convertView;
+    }
+
+
+
+    // Selections
+
+    /**
+     * Called when item is selected in Action mode
+     * @param position clicked file position in list
+     */
+    void itemSelect(int position){
+        boolean isSelected = false;
+        for (String fileName : selectedItems) {
+            if(fileName.equals(((File)getItem(position)).getName())){
+                isSelected = true;
+                break;
+            }
+        }
+        if(!isSelected)
+            selectedItems.add(((File)getItem(position)).getName());
+        else
+            selectedItems.remove(((File)getItem(position)).getName());
+
+        notifyDataSetChanged();
+    }
+
+    /**
+     * Removes all selected files (-> when setting Action mode off)
+     */
+    void removeSelection(){
+        selectedItems = new HashSet<>();
+        notifyDataSetChanged();
+    }
+
+    /**
+     * Returns files selected in Action mode
+     * @return selected files set
+     */
+    Set<String> getSelectedItems(){
+        return selectedItems;
+    }
+
+    /**
+     * Returns selected files count
+     * @return selected files count
+     */
+    int getSelectedCount(){
+        return selectedItems.size();
+    }
+}
---
+++ SimpleFileManager/app/src/main/java/com/example/SimpleFileManager/MainActivity.java
@@ -0,0 +1,273 @@
+package com.example.SimpleFileManager;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Environment;
+import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.view.ActionMode;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.ScaleAnimation;
+import android.webkit.MimeTypeMap;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
+import android.widget.Toast;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity {
+    private AbsListView filesView;
+    private FileAdapter filesAdapter;
+    private File currentFolder;
+    private File defaultFolder;
+    private static final String TAG = "MainActivity";
+
+    private Animation fadeOut;
+    private AnimationSet animationIn;
+
+    private ActionMode actionMode;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        filesView = findViewById(R.id.files_view);
+
+        if(Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)){
+            if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                    != PackageManager.PERMISSION_GRANTED){
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,
+                                Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
+            }
+        }
+
+        setDefaultPath();
+        currentFolder = new File(defaultFolder.getAbsolutePath());
+        filesAdapter = new FileAdapter(currentFolder, currentFolder.getAbsolutePath()
+                .equals(defaultFolder.getAbsolutePath()), MainActivity.this);
+
+        setActionMode();
+
+        setAnimations();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        setDefaultPath();
+        refreshFiles();
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        unsetActionMode();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
+        if(grantResults[0] != PackageManager.PERMISSION_GRANTED || grantResults[1] != PackageManager.PERMISSION_GRANTED)
+            Toast.makeText(this, "The storage won't be accesible..", Toast.LENGTH_SHORT).show();
+    }
+
+
+
+    // Overflow menu
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.overflow_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.action_settings:
+                Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+                Log.i(TAG,"Directing to Settings activity..");
+                startActivity(intent);
+                return true;
+            case R.id.action_refresh:
+                refreshFiles();
+                Toast.makeText(this, "Files refreshed..", Toast.LENGTH_SHORT).show();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+    // Activity initialization
+
+    /**
+     * Inits default path from shared preferences
+     */
+    private void setDefaultPath(){
+        String internalStoragePath = getObbDir().getParentFile().getParentFile().getParentFile()
+                .getAbsolutePath();
+
+        defaultFolder = new File(PreferenceManager.getDefaultSharedPreferences(MainActivity.this).getString(
+                getResources().getString(R.string.default_folder_key),internalStoragePath));
+    }
+
+    /**
+     * Inits animations of files ListView/GridView during refreshing
+     */
+    private void setAnimations(){
+        fadeOut = new AlphaAnimation(1,0);
+        fadeOut.setDuration(50);
+        fadeOut.setFillAfter(true);
+
+        Animation scaleIn = new ScaleAnimation(0.95f, 1f, 0.95f, 1f,
+                Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        scaleIn.setStartOffset(50);
+        scaleIn.setDuration(150);
+        Animation fadeIn = new AlphaAnimation(0,1);
+        fadeIn.setStartOffset(50);
+        fadeIn.setDuration(100);
+        fadeIn.setFillAfter(true);
+
+        animationIn = new AnimationSet(true);
+        animationIn.addAnimation(scaleIn);
+        animationIn.addAnimation(fadeIn);
+    }
+
+    /**
+     * Inits on item clicks from files ListView/GridView, inits Action mode
+     */
+    private void setActionMode(){
+        filesView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if(actionMode != null)
+                    listItemSelect(position);
+                else{
+                    File item = (File)filesAdapter.getItem(position);
+                    if(item == null)
+                        return;
+                    if(item.isDirectory())
+                        setCurrentFolder(new File(currentFolder.getAbsolutePath() + File.separator +
+                                item.getName()));
+                    else{
+                        Uri uri = Uri.fromFile(item);
+                        String mime = MimeTypeMap.getSingleton().getMimeTypeFromExtension(
+                                MimeTypeMap.getFileExtensionFromUrl(uri.toString()));
+                        Intent intent = new Intent();
+                        intent.setAction(Intent.ACTION_VIEW);
+                        intent.setDataAndType(uri,mime);
+                        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                        Log.i(TAG,"Opening file: " + item.getPath());
+                        startActivity(intent);
+                    }
+                }
+            }
+        });
+        filesView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                listItemSelect(position);
+                return true;
+            }
+        });
+    }
+
+
+
+    // Files list changing
+
+    /**
+     * Refreshes files ListView/GridView
+     */
+    void refreshFiles(){
+        unsetActionMode();
+        new Runnable() {
+            @Override
+            public void run() {
+                filesView.startAnimation(fadeOut);
+
+                filesAdapter = new FileAdapter(currentFolder, currentFolder.getAbsolutePath()
+                        .equals(defaultFolder.getAbsolutePath()), MainActivity.this);
+                filesView.setAdapter(filesAdapter);
+
+                filesView.startAnimation(animationIn);
+            }
+        }.run();
+        Log.i(TAG,"Files refreshed");
+    }
+
+    /**
+     * Sets current folder to entered
+     * @param folder folder to be current
+     */
+    void setCurrentFolder(File folder){
+        this.currentFolder = folder;
+        Log.i(TAG,"Current folder: " + folder.getAbsolutePath());
+        refreshFiles();
+    }
+
+    /**
+     * Sets deafult folder as current
+     */
+    void setDefaultFolder(){
+        this.currentFolder = new File(defaultFolder.getAbsolutePath());
+        Log.i(TAG,"Current folder: " + defaultFolder.getAbsolutePath());
+        refreshFiles();
+    }
+
+
+
+    // Methods for Action mode (CAB)
+
+    /**
+     * Called when Action mode is on or is to be set on
+     * @param position clicked file position in list
+     */
+    private void listItemSelect(int position){
+        filesAdapter.itemSelect(position);
+        boolean hasCheckedItems = filesAdapter.getSelectedCount() > 0;
+        if(hasCheckedItems && actionMode == null)
+            actionMode = startSupportActionMode(new ActionModeCallback(this,filesAdapter,
+                    currentFolder.getAbsolutePath()));
+        else if(!hasCheckedItems && actionMode != null)
+            actionMode.finish();
+
+        if(actionMode != null)
+            actionMode.setTitle(filesAdapter.getSelectedCount() + " selected items");
+    }
+
+    /**
+     * Disbales Action mode
+     */
+    void unsetActionMode(){
+        filesAdapter.removeSelection();
+        if(actionMode != null)
+            actionMode = null;
+    }
+
+    /**
+     * Returns Action mode object
+     * @return action mode
+     */
+    ActionMode getActionMode(){
+        return actionMode;
+    }
+}
